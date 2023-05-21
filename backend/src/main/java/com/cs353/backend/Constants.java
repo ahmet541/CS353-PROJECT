336f6735123cabf265dbda4756249ec7ea0b7609
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-05-21 14:46:56 +0300

    Constants added

---
+++ backend/src/main/java/com/cs353/backend/Constants.java
@@ -0,0 +1,7 @@
+package com.cs353.backend;
+
+public class Constants {
+    // Error messages
+    public static final String EMAIL_IN_USE_ERROR_MESSAGE = "This email is already in use.";
+    // Add more constants as needed
+}
