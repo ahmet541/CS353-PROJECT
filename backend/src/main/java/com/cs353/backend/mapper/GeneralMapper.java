2f246943659d7a5d8a28e9d1d96e5ff4373ca737
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-05-21 14:48:24 +0300

    Mappers added to convert particular type of model to another

---
+++ backend/src/main/java/com/cs353/backend/mapper/GeneralMapper.java
@@ -0,0 +1,44 @@
+package com.cs353.backend.mapper;
+
+import com.cs353.backend.model.entities.Account;
+import com.cs353.backend.model.entities.Company;
+import com.cs353.backend.model.entities.RegularUser;
+import com.cs353.backend.model.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GeneralMapper {
+
+    public Account mapRegularUserToAccount(RegularUser regularUser) {
+        Account account = new Account();
+        account.setEmail(regularUser.getEmail());
+        account.setPassword(regularUser.getPassword());
+
+        return account;
+    }
+
+    public Account mapCompanyToAccount(Company company) {
+        Account account = new Account();
+        account.setEmail(company.getEmail());
+        account.setPassword(company.getPassword());
+
+        return account;
+    }
+
+    public Account mapUserToAccount(User user) {
+        Account account = new Account();
+        account.setEmail(user.getEmail());
+        account.setPassword(user.getPassword());
+
+        return account;
+    }
+
+    public User mapRegularUserToUser(RegularUser regularUser) {
+        User user = new User();
+        user.setEmail(regularUser.getEmail());
+        user.setPassword(regularUser.getPassword());
+        user.setProfile_description(regularUser.getProfile_description());
+
+        return user;
+    }
+}
---
+++ backend/src/main/java/com/cs353/backend/mapper/RegularUserMapper.java
@@ -0,0 +1,27 @@
+package com.cs353.backend.mapper;
+
+import com.cs353.backend.model.entities.Account;
+import com.cs353.backend.model.entities.RegularUser;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RegularUserMapper implements RowMapper<RegularUser> {
+
+    @Override
+    public RegularUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+        RegularUser regularUser = new RegularUser();
+
+        regularUser.setId(rs.getInt("id"));
+        regularUser.setEmail(rs.getString("email"));
+        regularUser.setPassword(rs.getString("password"));
+        regularUser.setFirstName(rs.getString("first_name"));
+        regularUser.setLastName(rs.getString("last_name"));
+        regularUser.setGender(rs.getString("gender"));
+        regularUser.setPhoneNumber(rs.getString("phone_number"));
+        regularUser.setBirthdate(rs.getDate("birthdate"));
+        regularUser.setAddress(rs.getString("address"));
+        return regularUser;
+    }
+}
