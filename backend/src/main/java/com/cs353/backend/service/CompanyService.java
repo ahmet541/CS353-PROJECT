fb0b9bd8bf04166a4259fde7e937af7cc52cebef
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-05-21 14:49:58 +0300

    Company / User  services added

---
+++ backend/src/main/java/com/cs353/backend/service/CompanyService.java
@@ -0,0 +1,17 @@
+package com.cs353.backend.service;
+
+import com.cs353.backend.model.entities.Company;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+    public int createCompany(Company company) {
+        return 0;//TODO
+    }
+
+}
---
+++ backend/src/main/java/com/cs353/backend/service/UserService.java
@@ -0,0 +1,29 @@
+package com.cs353.backend.service;
+
+import com.cs353.backend.dao.AccountDao;
+import com.cs353.backend.dao.UserDao;
+import com.cs353.backend.mapper.GeneralMapper;
+import com.cs353.backend.model.entities.Account;
+import com.cs353.backend.model.entities.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserDao userDao;
+    private final AccountService accountService;
+    private final GeneralMapper generalMapper;
+
+    public int createUser(User user) {
+        Account account = generalMapper.mapUserToAccount(user);
+        int id = accountService.createAccount(account);
+        user.setId(id);
+        userDao.createUser(user);
+        return id;
+    }
+
+    public void updateAccount(User user) {
+        userDao.updateUser(user);
+    }
+}
