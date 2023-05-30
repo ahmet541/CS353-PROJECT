852efd2e264c62f773361023c49acc2fd07a94b1
Erkin-Aydin <erkinresearch@gmail.com>
2023-05-30 14:24:07 +0300

    Field entity has been created.

---
+++ backend/src/main/java/com/cs353/backend/model/entities/Field.java
@@ -0,0 +1,12 @@
+package com.cs353.backend.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Field {
+    private String field_name;
+}
