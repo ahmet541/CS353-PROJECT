1817baddabde9f6cf617e31e1a05ef632f156f64
KkubilayY <kemalkubilayyilmaz@gmail.com>
2023-06-01 02:14:51 +0300

    JobOpeningDAS func created

--- backend/src/main/java/com/cs353/backend/dao/service/JobOpeningDataAccessServer.java
+++ backend/src/main/java/com/cs353/backend/dao/service/JobOpeningDataAccessServer.java
@@ -1,4 +1,15 @@
 package com.cs353.backend.dao.service;
 
-public class JobOpeningDataAccessServer {
+import com.cs353.backend.dao.JobOpeningDao;
+import com.cs353.backend.model.entities.JobOpening;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
+public class JobOpeningDataAccessServer implements JobOpeningDao{
+    @Override
+    public JobOpening createJobOpening(JobOpening jobOpening, int userId) {
+        return null;
+    }
 }
