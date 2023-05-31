f1fd457b7473e50960ea83f987c63c6458044f60
KkubilayY <kemalkubilayyilmaz@gmail.com>
2023-06-01 02:50:27 +0300

    createJobOpening implemented

--- backend/src/main/java/com/cs353/backend/dao/service/JobOpeningDataAccessServer.java
+++ backend/src/main/java/com/cs353/backend/dao/service/JobOpeningDataAccessServer.java
@@ -3,13 +3,23 @@ package com.cs353.backend.dao.service;
 import com.cs353.backend.dao.JobOpeningDao;
 import com.cs353.backend.model.entities.JobOpening;
 import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @AllArgsConstructor
 public class JobOpeningDataAccessServer implements JobOpeningDao{
+
+    private JdbcTemplate jdbcTemplate;
     @Override
     public JobOpening createJobOpening(JobOpening jobOpening, int userId) {
-        return null;
+        String sql = """
+                INSERT INTO JobOpening (employment_status, explanation, due_date, role_pro, location, work_type)
+                VALUES (?, ?, ?, ?, ?, ?)
+                RETURNING job_opening_id
+                """;
+        int id = jdbcTemplate.queryForObject(sql, Integer.class, jobOpening.getEmploymentStatus(), jobOpening.getExplanation(), jobOpening.getDueDate(), jobOpening.getRolePro(), jobOpening.getLocation(), jobOpening.getWorkType() );
+        jobOpening.setJobOpeningID(id);
+        return jobOpening;
     }
 }
