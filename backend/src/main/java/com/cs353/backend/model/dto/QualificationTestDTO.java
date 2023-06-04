e2f85ce220340ed51cf943ce60e20734a1f48865
Erkin-Aydin <erkinresearch@gmail.com>
2023-06-04 18:18:08 +0300

    QualificationTestDTO filled.

--- backend/src/main/java/com/cs353/backend/model/dto/QualificationTestDTO.java
+++ backend/src/main/java/com/cs353/backend/model/dto/QualificationTestDTO.java
@@ -4,8 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Time;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class QualificationTestDTO {
+    private int     job_opening_id;
+    private String  instructions;
+    private Time given_time;
 }
